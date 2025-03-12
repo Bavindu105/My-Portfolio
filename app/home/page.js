8ae9446100dc92a6477cc5a0c3a39e541fe29d61
Bavindu105 <bavinduashan@gmail.com>
2025-03-13 05:03:19 +0530

    Hero start

--- app/home/page.js
+++ app/home/page.js
@@ -1,13 +1,26 @@
+import Image from "next/image"
+
 export default function HomePage() {
-    return(
+    return (
         <>
-        <div className="flex flex-col justify-center items-center w-full z-0">
-            <div className="flex flex-row items-center w-[1280px] h-screen">
-                <div>
-                    <h1 className="text-[80px] font-bold ">Bavindu Ashan</h1>
+            {/* Hero section */}
+            <div className="flex flex-col justify-center items-center w-full h-screen z-0">
+                <div className="flex absolute bottom-0 justify-center items-center w-[1280px] h-screen">
+                    <h1 className="text-[280px] font-bold text-[#4b4b4b8e]">Bavindu</h1>
+                    <div className="flex w-full h-[80vh] absolute bottom-0">
+                        <Image
+                            src="/images/dummy-person.png"
+                            alt=""
+                            fill
+                            objectFit="contain"
+
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
+
+            {/* About section */}
+            {/* <div>aaa</div> */}
         </>
     )
 }
--- components/header.js
+++ components/header.js
@@ -2,14 +2,14 @@ export default function Header() {
     return (
         <div className="flex justify-center w-full mb-[-100px]">
             <div className="flex flex-row items-center justify-between w-[1280px] h-[100px] font-medium text-[16px] z-[100]">
-                <div className="w-[100px] h-[100px] bg-amber-300"></div>
-                <ul className="flex flex-row gap-20 text-white">
+                <div className="flex items-center font-bold text-white text-[30px]">LOGO</div>
+                <ul className="flex flex-row items-center justify-center gap-20 text-white">
                     <li><a href="#">Home</a></li>
+                    <li><a href="#">Porfolio</a></li>
                     <li><a href="#">About</a></li>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Contact</a></li>
                 </ul>
-                <button className="py-2 px-6 bg-[#810CA8] hover:bg-[#740B97] rounded-[20px]">Button</button>
+                <button className="py-2 px-6 bg-[#810CA8] hover:bg-[#740B97] rounded-[20px]">Download Resume</button>
             </div>
         </div>
     )
